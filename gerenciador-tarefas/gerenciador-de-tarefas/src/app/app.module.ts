32ecc6e7b098a9a69430b0a544f29ca9afd68cdb
Giovanni Ribeiro Pinto <giovanni.ribeiro@grupoltm.com.br>
2018-07-20 18:45:46 -0300

    Adicionando Component e criando model

--- gerenciador-tarefas/gerenciador-de-tarefas/src/app/app.module.ts
+++ gerenciador-tarefas/gerenciador-de-tarefas/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TarefasModule } from './tarefas';
 
 
 @NgModule({
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule, 
+    AppRoutingModule,
+    TarefasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
