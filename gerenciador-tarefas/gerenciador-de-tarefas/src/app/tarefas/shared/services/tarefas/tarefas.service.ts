9fb838a075a76231949460ddf1c75ca50efe0a31
Giovanni Ribeiro Pinto <giovanni.ribeiro@grupoltm.com.br>
2018-07-26 12:43:51 -0300

    adicionando diretorio de service e criando regras de negocio

---
+++ gerenciador-tarefas/gerenciador-de-tarefas/src/app/tarefas/shared/services/tarefas/tarefas.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Tarefa } from '../../tarefa.model';
+
+@Injectable()
+export class TarefasService {
+
+  constructor() { }
+
+  listarTodos(): Tarefa[] {
+    const tarefas = localStorage['tarefas'];
+    return tarefas ? JSON.parse(tarefas) : [];
+  }
+
+  cadastrar(tarefa: Tarefa): void {
+    const tarefas = this.listarTodos();
+    tarefa.id = new Date().getTime();
+    tarefas.push(tarefa);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+
+
+
+}
--- gerenciador-tarefas/gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
+++ gerenciador-tarefas/gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TarefasService } from './shared';
+
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [
+    TarefasService
+  ]
 })
 export class TarefasModule { }
