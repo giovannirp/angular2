7f8d11c5b158d484acc155c15bc33c9da2fba266
Giovanni Ribeiro Pinto <giovanni.ribeiro@grupoltm.com.br>
2018-07-27 18:43:51 -0300

    adicionando alteracao de module

--- gerenciador-tarefas/gerenciador-de-tarefas/src/app/tarefas/listar/listar-tarefa.component.ts
+++ gerenciador-tarefas/gerenciador-de-tarefas/src/app/tarefas/listar/listar-tarefa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TarefasService, Tarefa } from   '../shared';
 
 @Component({
   selector: 'app-listar-tarefa',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarTarefaComponent implements OnInit {
 
-  constructor() { }
+  tarefas: Tarefa[];
+
+  constructor(private tarefaService: TarefasService) { }
 
   ngOnInit() {
+    this.tarefas = this.listarTodos();
+  }
+
+  listarTodos(): Tarefa[] {
+    return this.tarefaService.listarTodos();
   }
 
 }
