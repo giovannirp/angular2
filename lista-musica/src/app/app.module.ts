b93cc1f457f56a16a7b97904319e62b5989311ec
giovanni <j0h@example.com>
2018-10-06 19:56:22 -0300

    acertando layout do projeto

--- lista-musica/src/app/app.module.ts
+++ lista-musica/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    TesteComponent
   ],
   imports: [
     BrowserModule
