50e5dca45fe62c32fc52ce6f07470a43303cb093
giovanni <j0h@example.com>
2018-10-06 21:30:11 -0300

    acertando projeto e adicionando

--- lista-musica/src/app/app.module.ts
+++ lista-musica/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import {ROUTES} from './app.routes';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { TesteComponent } from './teste/teste.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    TesteComponent
+    AboutComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
