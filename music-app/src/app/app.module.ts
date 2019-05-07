8a50d3d4accf452d880f9a508106634300740200
giovanni <j0h@example.com>
2019-05-07 08:14:39 -0300

    acertando component de fale conosco

--- music-app/src/app/app.module.ts
+++ music-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { ROUTES } from './app.routes';
 
@@ -45,6 +46,7 @@ import { ContactComponent } from './contact/contact.component';
   imports: [
     BrowserModule,
     HttpModule,
+    FormsModule,
     RouterModule.forRoot(ROUTES)
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
