32a7986a7f84a65a8b97d7810de445c5ca07610e
Giovanni Ribeiro Pinto <giovanni.ribeiro@grupoltm.com.br>
2019-05-17 17:23:10 -0300

    acertando layout novo e adicionando sass

--- project-new/src/app/shared/components/cards/cards.component.ts
+++ project-new/src/app/shared/components/cards/cards.component.ts
@@ -9,6 +9,27 @@ export class CardsComponent implements OnInit {
 
   constructor() { }
 
+  array = [
+    {
+      name: 'Avon Store',
+      desconto: '5% de desconto',
+      imgPath: 'assets/img/img-box.png',
+      description: '5% de desconto através do hotsite da parceria. Não cumulativo com outras promoções',
+    },
+    {
+      name: 'Extra',
+      desconto: '13% de desconto',
+      imgPath: 'assets/img/img-box.png',
+      description: '0% de desconto através do hotsite da parceria. Não cumulativo com outras promoções',
+    },
+    {
+      name: 'Onofre',
+      desconto: '3% de desconto',
+      imgPath: 'assets/img/img-box.png',
+      description: '1% de desconto através do hotsite da parceria. Não cumulativo com outras promoções',
+    },
+  ];
+
   ngOnInit() {
   }
 
