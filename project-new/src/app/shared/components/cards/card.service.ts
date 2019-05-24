0fb6465afcc41ba8d6b97df8cf6ede3b74b3215b
Giovanni Ribeiro Pinto <giovanni.ribeiro@grupoltm.com.br>
2019-05-24 15:02:21 -0300

    tratando mensagem de erro

--- project-new/src/app/shared/components/cards/card.service.ts
+++ project-new/src/app/shared/components/cards/card.service.ts
@@ -2,23 +2,35 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MEAT_API } from '../../../app.api';
 import { Card } from './card/card.model';
-import { Observable } from 'rxjs';
-import { map, catchError, tap } from 'rxjs/operators';
-
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { ErrorHandler } from '../../../app.error-handler';
 
 @Injectable()
 export class CardService {
-
-  private extractData(res: Response) {
-    const body  = res;
-    return body || { };
-  }
+  /*
+    private extractData(res: Response) {
+      const body  = res;
+      return body || { };
+    }
+    */
 
   constructor(private http: HttpClient) { }
 
+  /*
   getCards(): Observable<any> {
     return this.http.get(MEAT_API + '/cards').pipe(
       map(this.extractData));
   }
+ */
+  getCards(): Observable<any> {
+    return this.http.get(MEAT_API + '/cards')
+    .pipe(
+      catchError(ErrorHandler.handlerError)
+    );
+  }
+
+
+
 
 }
