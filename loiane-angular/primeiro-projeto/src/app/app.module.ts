ab95ac86547d319f0d43de42b5aa8ceeff459946
giovanni <j0h@example.com>
2019-06-05 08:48:20 -0300

    adicionando projeto do curso

---
+++ loiane-angular/primeiro-projeto/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuPrimeiro2Component
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
