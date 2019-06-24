3591b9a894287a735ccadad4562eb48586ccd95e
Giovanni Ribeiro Pinto <giovanni.ribeiro@grupoltm.com.br>
2019-06-24 19:16:27 -0300

    modelando

--- devs/src/app/models/details.model.ts
+++ devs/src/app/models/details.model.ts
@@ -2,5 +2,12 @@ export interface Details {
   id: number;
   login: string;
   avatar_url: string;
+  html_url: string;
   repos_url: string;
+  received_events_url: string;
+  type: string;
+  url: string;
+  created_at: Date;
+  updated_at: Date;
+  public_repos: number;
 }
