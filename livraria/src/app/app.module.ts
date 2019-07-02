5d66ec323e29f4019dba7cc3f799d5cf8b68dbef
Giovanni Ribeiro Pinto <giovanni.ribeiro@grupoltm.com.br>
2019-07-02 19:45:55 -0300

    add projeto

---
+++ livraria/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    AppRoutingModule
+  ],
+  exports: [BsDropdownModule, TooltipModule, ModalModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
